49150b64a62e0223de8bb9059de0e1a639c6330f
Pthumeriano <38404540+Pthumeriano@users.noreply.github.com>
2022-10-19 21:24:29 -0300

    Update Livro.java

--- Lista1AV2/src/Livro.java
+++ Lista1AV2/src/Livro.java
@@ -3,6 +3,59 @@ import java.util.Date;
 public class Livro {
 	
 	private String codigo;
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getPrazo() {
+		return prazo;
+	}
+
+	public void setPrazo(int prazo) {
+		this.prazo = prazo;
+	}
+
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	public int getEdicao() {
+		return edicao;
+	}
+
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
+
+	public boolean isAlugado() {
+		return alugado;
+	}
+
+	public void setAlugado(boolean alugado) {
+		this.alugado = alugado;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
 	private String titulo;
 	private String autor;
 	private int prazo;
@@ -33,21 +86,13 @@ public class Livro {
 		this.prazo = -1;
 	}
 	
-	public boolean estaAlugado() {
-		return this.alugado;
-	}
-	
-	public void foiAlugado() {
-		this.alugado = !alugado;
-	}
-
 	public Pessoa getQuemAlugouPorUltimo() {
 		return quemAlugouPorUltimo;
 	}
 
 	public void setQuemAlugouPorUltimo(Pessoa quemAlugouPorUltimo) {
 		this.quemAlugouPorUltimo = quemAlugouPorUltimo;
-		if(quemAlugouPorUltimo.getClass().getSimpleName()=="Prfoessor") {
+		if(quemAlugouPorUltimo.getClass().getSimpleName()=="Professor") {
 			this.prazo = 30;
 		}else {
 			this.prazo = 10;
